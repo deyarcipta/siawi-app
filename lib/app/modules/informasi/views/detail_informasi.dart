d42042ff9d9cbe083e7541c221562cb3028ff078
deyarcipta <deyar.cipta@gmail.com>
2024-05-11 08:32:29 +0700

    aplikasi release dengan api laravel

---
+++ lib/app/modules/informasi/views/detail_informasi.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:siawi_app/app/modules/informasi/widget/informasi.dart';
+import 'package:siawi_app/utils/colors.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+
+class DetailInformasi extends StatelessWidget {
+  late String fileURL;
+
+  DetailInformasi({required this.fileURL}) {
+    // Lakukan encoding URL di sini setelah nilai fileURL diinisialisasi
+    this.fileURL = Uri.encodeComponent(fileURL);
+  }
+
+  var appBarHeight = AppBar().preferredSize.height;
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.mainColor,
+        centerTitle: true,
+        leading: BackButton(color: AppColors.white),
+        title: Text(
+          'SI AWI',
+          style: TextStyle(color: AppColors.white),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+            child: Image.asset(
+              'assets/logo/logo-splash.png',
+              width: 30,
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: SfPdfViewer.network(
+                  'http://203.194.113.46/storage/file-informasi/$fileURL',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/modules/password/views/password_view.dart
@@ -0,0 +1,231 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:siawi_app/app/modules/home/views/MyHomePage.dart';
+import 'package:siawi_app/app/modules/home/views/home_view.dart';
+import 'package:siawi_app/utils/colors.dart';
+import 'package:http/http.dart' as http;
+// import 'package:fluttertoast/fluttertoast.dart';
+
+class PasswordView extends StatefulWidget {
+  final VoidCallback signOut;
+  const PasswordView(this.signOut, {Key? key}) : super(key: key);
+
+  @override
+  State<PasswordView> createState() => _PasswordViewState();
+}
+
+class _PasswordViewState extends State<PasswordView> {
+  late String password1;
+  late String password2;
+  late String? _idSiswa;
+  final _formKey = GlobalKey<FormState>();
+
+  Future<String?> _loadIdSiswa() async {
+    final idSiswa = await _getIdSiswa();
+    setState(() {
+      _idSiswa = idSiswa;
+    });
+    return idSiswa;
+  }
+
+  Future<String?> _getIdSiswa() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getString('idSiswa');
+  }
+
+  void gantiPassword(String idSiswa) async {
+    final response = await http
+        .post(Uri.parse('http://203.194.113.46/api/ubahPassword'), body: {
+      'idSiswa': idSiswa,
+      'password1': password1,
+      'password2': password2,
+    });
+
+    // print('password : ${response.statusCode}');
+    if (response.statusCode == 200) {
+      var jsonResponse = json.decode(response.body);
+      if (jsonResponse['success']) {
+        // Jika login berhasil, akses data pengguna
+        // Fluttertoast.showToast(
+        //   msg: 'Password Berhasil Diganti',
+        //   backgroundColor: Colors.green,
+        //   textColor: Colors.white,
+        //   toastLength: Toast.LENGTH_SHORT,
+        // );
+
+        // Navigasi ke halaman lain setelah menampilkan pesan toast
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => HomeView(widget.signOut)),
+        );
+      } else {
+        // Fluttertoast.showToast(
+        //   msg: 'Gagal Mengganti Password',
+        //   backgroundColor: Colors.red,
+        //   textColor: Colors.white,
+        //   toastLength: Toast.LENGTH_SHORT,
+        // );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.mainColor,
+        centerTitle: true,
+        leading: BackButton(color: AppColors.white),
+        title: Text(
+          'SI AWI',
+          style: TextStyle(color: AppColors.white),
+        ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+            child: Image.asset(
+              'assets/logo/logo-splash.png',
+              width: 30,
+            ),
+          )
+        ],
+      ),
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // const SizedBox(
+              //   height: 50,
+              // ),
+              Container(
+                width: 125,
+                height: 125,
+                child: Image.asset(
+                  "assets/logo/logo-splash.png",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
+                child: SizedBox(
+                  height: 72,
+                  width: 320,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter password';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) => password1 = value!,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: AppColors.white,
+                      contentPadding: const EdgeInsets.all(0),
+                      hintText: "Password Baru",
+                      prefixIcon: Container(
+                        padding: const EdgeInsets.only(left: 20, right: 15),
+                        child: const Icon(
+                          Icons.lock,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: AppColors.mainColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: SizedBox(
+                  height: 50,
+                  width: 320,
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter password again';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) => password2 = value!,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: AppColors.white,
+                      contentPadding: const EdgeInsets.all(0),
+                      hintText: "Ulangi Password Baru",
+                      prefixIcon: Container(
+                        padding: const EdgeInsets.only(left: 20, right: 15),
+                        child: Icon(
+                          Icons.lock,
+                        ),
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(
+                          width: 2,
+                          color: AppColors.mainColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          _loadIdSiswa().then((idSiswa) {
+                            if (idSiswa != null) {
+                              gantiPassword(idSiswa);
+                            } else {
+                              // Fluttertoast.showToast(
+                              //   msg: 'Gagal memuat ID siswa',
+                              //   backgroundColor: Colors.red,
+                              //   textColor: Colors.white,
+                              //   toastLength: Toast.LENGTH_SHORT,
+                              // );
+                            }
+                          });
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.mainColor,
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        fixedSize: const Size(200, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30)),
+                      ),
+                      child: Text(
+                        "SIMPAN",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
